64fbe389a0eb50fdefedbdff6c010f485adbbe93
sgm1n0 <139188008+sungmin8619@users.noreply.github.com>
2026-08-06 20:00:35 +0900

    Update remove.java

--- remove.java
+++ remove.java
@@ -1,30 +1,48 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
-        class Board {
-            int id;
-            String title;
-            String content;
-            String writer;
-            String date;
-        }
+class Board {
+    int id;
+    String title;
+    String content;
+    String writer;
+    String date;
+}
         
-        ArrayList<Board> BoardList = new ArrayList<>();
+class BoardManager{
+    static ArrayList<Board> boardList = new ArrayList<>();
+}
 
+public class Remove{
+    public static void remove(ArrayList<Board> boardList){
         if(menu == 4){
+            if(boardList.size() == 0){
+                System.out.println("삭제할 수 있는 게시물이 없습니다.\n");
+                return;
+            }
             int target_idx = -1;
             Scanner sc = new Scanner(System.in);
-            System.out.print("삭제하실 게시물의 번호(ID)를 입력해주세요 : ");
-            int want_id = sc.nextInt();
-            for(int i = 0;i < BoardList.size();i++){
-                if(want_id == BoardList.get(i).id){
+            System.out.print("삭제하실 게시물의 번호를 입력해주세요 : ");
+            int want_idx = sc.nextInt();
+            sc.nectline();
+            if(want_idx <= 0){
+                System.out.println("올바른 번호를 입력해주세요.\n");
+                return;
+            }
+            for(int i = 0;i < boardList.size();i++){
+                if(want_idx == boardList.get(i).id){
                     target_idx = i;
                     break;
                 }
             }
-            BoardList.remove(target_idx);
+            if(target_idx == -1){
+                System.out.println("해당 게시물이 없습니다.\n");
+                return;
+            }
+            boardList.remove(target_idx);
+            for(int i = target_idx;i < boardList.size();i++){
+                boardList.get(i).id--;
+            }
         }
     }
 }
