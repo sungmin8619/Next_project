24868759f2c6ee8825af50cc8eb89d42919afc00
reve <leejen2926@gmail.com>
2026-08-13 14:34:55 +0900

    Update visit.java
    수정 완료

--- visit.java
+++ visit.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Visit{
-    public static void main(String[] args){
+    public static void visit(ArrayList<Board> boardList){
 
         Scanner sc = new Scanner(System.in);
 
@@ -10,24 +10,24 @@ class Visit{
         String yn = sc.nextLine();
 
         if(yn.equals("y")){
-            int k = BoardManager.boardList.size();
+            int k = boardList.size();
             int n = 0;
             boolean d = false;
             System.out.println("What's title ?");   //어떤 게시글 조회?
             String ti = sc.nextLine();              //제목 입력 받음
 
             for(int i = 0; i < k; i++) {           //title에 맞는 id 찾기
-                if (ti.equals(BoardManager.boardList.get(i).title)){
+                if (ti.equals(boardList.get(i).title)){
                     d = true;
-                    n = BoardManager.boardList.get(i).id;
+                    n = boardList.get(i).id;
                 }
             }
             if(!d){
                 System.out.println("error !");  //입력 받은 제목과 게시글의 제목이 다르면 error 표시
             }
             if(d) {
-                System.out.println(BoardManager.boardList.get(n).id + BoardManager.boardList.get(n).writer + BoardManager.boardList.get(n).title + BoardManager.boardList.get(n).date);
-                System.out.println(BoardManager.boardList.get(n).content);          //타이틀에 연결된 내용 불러옴
+                System.out.println(boardList.get(n).id + boardList.get(n).writer + boardList.get(n).title + boardList.get(n).date);
+                System.out.println(boardList.get(n).content);          //타이틀에 연결된 내용 불러옴
             }
         }
     }
