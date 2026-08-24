4c99d167cc621533043314cf3e1b3133d1f4e294
yunjeongbeen <jeongbeen@mju.ac.kr>
2026-08-24 16:40:54 +0900

    Update Create.java

--- Create.java
+++ Create.java
@@ -12,34 +12,46 @@ public class Create {
         // id 생성
         board.id = boardList.size() + 1;
 
-        // 제목 입력
-        System.out.println("제목: ");
-        board.title = scanner.nextLine();
+        while(true) {
+            // 제목 입력
+            System.out.println("제목: ");
+            board.title = scanner.nextLine();
 
-        // 제목이 공란일 경우
-        if(board.title.isBlank()){
-            System.out.println("제목은 비워둘 수 없습니다.");
-            return;
+            // 제목이 공란일 경우
+            if(board.title.isBlank()){
+                System.out.println("제목은 비워둘 수 없습니다.");
+                return;
+            } else { // 제목 입력 -> 넘어가기
+                break;
+            }
         }
 
-        // 내용 입력
-        System.out.println("내용: ");
-        board.content = scanner.nextLine();
+        while(true) {
+            // 내용 입력
+            System.out.println("내용: ");
+            board.content = scanner.nextLine();
 
-        // 내용이 공란일 경우
-        if(board.content.isBlank()){
-            System.out.println("내용은 비워둘 수 없습니다.");
-            return;
+            // 내용이 공란일 경우
+            if(board.content.isBlank()){
+                System.out.println("내용은 비워둘 수 없습니다.");
+                return;
+            } else { // 내용 입력 -> 넘어가기
+                break;
+            }
         }
 
-        // 작성자
-        System.out.println("작성자: ");
-        board.writer = scanner.nextLine();
+        while(true) {
+            // 작성자
+            System.out.println("작성자: ");
+            board.writer = scanner.nextLine();
 
-        // 작성자가 공란일 경우
-        if(board.writer.isBlank()){
-            System.out.println("작성자는 비워둘 수 없습니다.");
-            return;
+            // 작성자가 공란일 경우
+            if(board.writer.isBlank()){
+                System.out.println("작성자는 비워둘 수 없습니다.");
+                return;
+            } else { // 작성자 입력 -> 넘어가기
+                break;
+            }
         }
 
         // 날짜 (현재 날짜)
@@ -51,4 +63,3 @@ public class Create {
         System.out.println("추가되었습니다.");
     }
 }
-
