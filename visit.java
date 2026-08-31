a767028a1452f797361679b6b3b0b92e09a23f80
reve <leejen2926@gmail.com>
2026-08-31 11:36:12 +0900

    Update visit.java
    public 바꿈

--- visit.java
+++ visit.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Visit{
+public class Visit{
     public static void visit(ArrayList<Board> boardList){
 
         Scanner sc = new Scanner(System.in);
