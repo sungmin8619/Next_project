afdd725406f023efdbdc3a2a7de9d258f9453a1d
sgm1n0 <139188008+sungmin8619@users.noreply.github.com>
2026-08-31 19:59:09 +0900

    Update Back_Up.java

--- Back_Up.java
+++ Back_Up.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Back_Up {
     public static ArrayList<Board> backUpList = new ArrayList<>();
     public static void saveBackup(Board board) {
@@ -8,29 +11,36 @@ public class Back_Up {
             System.out.println("불러올 수 있는 게시물이 없습니다.\n");
             return;
         }
+        for(int i = 0;i < backUpList.size();i++){
+            System.out.println("글 번호 : " + backUpList.get(i).id + "제목 : " + backUpList.get(i).title + "작성자 : " + backUpList.get(i).writer);
+        }
         Scanner sc = new Scanner(System.in);
-        System.out.print("불러오실 게시물의 번호를 입력해주세요 : ");
-        int want_idx = -1;
-        try {
-            want_idx = Integer.parseInt(sc.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("숫자만 입력해 주세요.\n");
-            return;
-        }
-        if (want_idx <= 0) {
-            System.out.println("올바른 번호를 입력해주세요.\n");
-            return;
-        }
         int target_idx = -1;
-        for (int i = 0; i < backUpList.size(); i++) {
-            if (want_idx == backUpList.get(i).id) {
-                target_idx = i;
-                break;
+        while(true){
+            System.out.print("불러오실 게시물의 번호를 입력해주세요 : ");
+            int want_idx = -1;
+            try{
+                want_idx = Integer.parseInt(sc.nextLine());
+            }catch(NumberFormatException e) {
+                System.out.println("숫자만 입력해 주세요.\n");
+                continue;
             }
-        }
-        if (target_idx == -1) {
-            System.out.println("해당 게시물이 없습니다.\n");
-            return;
+            if(want_idx <= 0) {
+                System.out.println("올바른 번호를 입력해주세요.\n");
+                continue;
+            }
+            target_idx = -1;
+            for(int i = 0; i < backUpList.size(); i++) {
+                if(want_idx == backUpList.get(i).id) {
+                    target_idx = i;
+                    break;
+                }
+            }
+            if(target_idx == -1) {
+                System.out.println("해당 게시물이 없습니다.\n");
+                continue;
+            }
+            break;
         }
         Board restoredBoard = backUpList.remove(target_idx);
         restoredBoard.id = boardList.size() + 1;
